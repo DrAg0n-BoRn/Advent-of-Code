9fde01ccbd739c641201645153876c7940ee87e0
Karl Loza <luigiloza@gmail.com>
2022-12-03 10:56:03 +0800

    Day 2 Part II

--- day2.py
+++ day2.py
@@ -17,43 +17,67 @@ lose = 0
 draw = 3
 win = 6
 
-
-def convert(choice: str) -> int:
-    match choice:
-        case "X":
-            return 1
-        case "Y":
-            return 2
-        case "Z":
-            return 3
-        case "A":
-            return 1
-        case "B":
-            return 2
-        case "C":
-            return 3
-
-
-def get_score(elf: str, player: str) -> int:
-    player: int = convert(player)
-    elf: int = convert(elf)
-    
-    if player == elf:
-        return player + draw
-    elif player == 1 and elf == 3:
-        return player + win
-    elif elf == 1 and player == 3:
-        return player + lose
-    elif player > elf:
-        return player + win
-    elif player < elf:
-        return player + lose
-
+rock = 1
+paper = 2
+scissors = 3
 
 score = 0
+
+# --- PART I --- 
+# def convert(choice: str) -> int:
+#     match choice:
+#         case "X":
+#             return rock
+#         case "Y":
+#             return paper
+#         case "Z":
+#             return scissors
+#         case "A":
+#             return rock
+#         case "B":
+#             return paper
+#         case "C":
+#             return scissors
+
+
+# def get_score(elf: str, player: str) -> int:
+#     player: int = convert(player)
+#     elf: int = convert(elf)
+
+#     if player == elf:
+#         return player + draw
+#     elif player == 1 and elf == 3:
+#         return player + win
+#     elif elf == 1 and player == 3:
+#         return player + lose
+#     elif player > elf:
+#         return player + win
+#     elif player < elf:
+#         return player + lose
+
+
+# with open("./inputs/input2.txt", "r") as file:
+#     for ronda in file:
+#         list_ = ronda.strip().split(sep=" ")
+#         score += get_score(elf=list_[0], player=list_[1])
+
+# print(score)
+
+
+# --- PART II ---
+magic_hat = {
+    # Win
+    "Z":{"A": paper + win, "B": scissors + win, "C": rock + win},
+    # Lose
+    "X":{"A": scissors + lose, "B": rock + lose, "C": paper + lose},
+    # Draw
+    "Y":{"A": rock + draw, "B": paper + draw, "C": scissors + draw},
+}
+
 with open("./inputs/input2.txt", "r") as file:
     for ronda in file:
-        list_ = ronda.strip().split(sep=" ")
-        score += get_score(elf=list_[0], player=list_[1])
-        
+        player = ronda.strip().split(sep=" ")[1]
+        elf = ronda.strip().split(sep=" ")[0]
+        score += magic_hat[player][elf]
+
 print(score)
